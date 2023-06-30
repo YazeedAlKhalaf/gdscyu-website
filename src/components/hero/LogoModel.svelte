9b86d48fc4ea4ff5ebec397ac38691428ae3797d
Khalil Melhem <khalil.i.melhem@gmail.com>
2023-06-30 21:07:00 +0300

    migrated to Threlte 6 && Svelte 4 :)

--- src/components/hero/LogoModel.svelte
+++ src/components/hero/LogoModel.svelte
@@ -1,17 +1,16 @@
 <script>
-  import { Canvas, PerspectiveCamera, OrbitControls } from "@threlte/core";
-  import { GLTF } from "@threlte/extras";
+  import { Canvas, T } from "@threlte/core";
+  import { GLTF, OrbitControls } from "@threlte/extras";
 </script>
 
 <div class="w-5/6 md:w-1/2 lg:min-h-[24.5rem] lg:w-full lg:max-w-xl">
   <Canvas>
     <!-- Camera -->
-    <PerspectiveCamera position={{ x: 75, y: 0, z: 50 }} fov={25}>
+    <T.PerspectiveCamera makeDefault position={[75, 0, 50]} fov={25}>
       <!-- Controls -->
-      <!-- **minDistance={85} with a scale of {5} -->
-      <OrbitControls autoRotate enableZoom={false} />
-    </PerspectiveCamera>
+      <OrbitControls autoRotate enableDamping enableZoom={false} />
+    </T.PerspectiveCamera>
     <!-- Logo -->
-    <GLTF useDraco={true} scale={4.5} url="/models/gdsc-logo.glb" />
+    <GLTF url="/models/gdsc-logo.glb" useDraco={true} scale={4.5} />
   </Canvas>
 </div>
